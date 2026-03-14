6bfa4ad558987360be341094d2a22e4ad56af60b
Ralph Gacusan <ralphjayrell.e.gacusan@gmail.com>
2026-03-15 05:23:18 +0800

    chore: initial railway setup

--- app/main.py
+++ app/main.py
@@ -88,3 +88,10 @@ async def ping():
         dict: {"ping": "pong"}
     """
     return {"ping": "pong"}
+
+
+if __name__ == "__main__":
+    import uvicorn
+    import os
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
